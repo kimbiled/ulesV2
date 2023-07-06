1a86169a1b369c719e49100dbde39fd8157e0ec1
sabyr-cpu <sab.sabyr@gmail.com>
2023-07-06 15:18:32 +0600

    search placeholder to russian

--- frontend/src/app/shop/search.tsx
+++ frontend/src/app/shop/search.tsx
@@ -5,7 +5,7 @@ export default function Search () {
         <path d="M7.66683 13.9999C11.1646 13.9999 14.0002 11.1644 14.0002 7.66659C14.0002 4.16878 11.1646 1.33325 7.66683 1.33325C4.16903 1.33325 1.3335 4.16878 1.3335 7.66659C1.3335 11.1644 4.16903 13.9999 7.66683 13.9999Z" stroke="#273040" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
         <path d="M14.6668 14.6666L13.3335 13.3333" stroke="#273040" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
       </svg>
-      <input type="text" placeholder="Search here..." className="border-0 bg-white text-sm h-16 w-[650px]" />
+      <input type="text" placeholder="Поиск..." className="border-0 bg-white text-sm h-16 w-[650px]" />
     </div>
   )
 }
