db39f41ca2729f2bdc08726337daeccd27bba37d
Meirambekov Nursultan <meirambekov.n2005@gmail.com>
2023-07-15 21:41:13 +0600

    fix modal

--- frontend/src/app/shop/profile.tsx
+++ frontend/src/app/shop/profile.tsx
@@ -108,7 +108,7 @@ export default function Profile() {
 									</label>
 									<input
 										className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-										placeholder="5"
+										placeholder="5 г/мл/шт"
 										required
 										ref={quantityPerUnitRef}
 									/>
