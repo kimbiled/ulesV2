6e4e43fd82f1942635301583ecc1907bc8a84e37
Meirambekov Nursultan <meirambekov.n2005@gmail.com>
2023-07-18 15:02:58 +0600

    OrderModal + delete orders tab in shop

--- frontend/src/app/shop/page.tsx
+++ frontend/src/app/shop/page.tsx
@@ -5,9 +5,7 @@ import Info from "./info";
 import Profile from "./profile";
 import Statistics from "./statistics";
 import Orders from "./orders";
-import { Plus_Jakarta_Sans } from "next/font/google";
 import { useState } from "react";
-const pjs = Plus_Jakarta_Sans({ subsets: ["latin"] });
 export default function Shop() {
 	const [page, setPage] = useState(1);
 
@@ -18,9 +16,9 @@ export default function Shop() {
 	return (
 		<>
 			<Header />
-			<div className={"flex h-[calc(100vh-80px)]" + pjs.className}>
+			<div className="flex h-[calc(100vh-80px) fontRaleway">
 				<Menu setPage={changePage} />
-				{page === 1 ? <Profile /> : page === 2 ? <Statistics /> : page === 3 ? <Orders /> : page === 4 ? <Info /> : null}
+				{page === 1 ? <Profile /> : page === 2 ? <Statistics /> : page === 3 ? <Info /> : null}
 			</div>
 		</>
 	);
