6a636aa95c275f24a758e3a08a51922ee8b0c2d6
Danial <danial_ikar@mail.ru>
2023-07-19 19:08:30 +0600

    Fixed front

--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -78,14 +78,14 @@ export default function Home() {
 									</div>
 									<div>
 										<label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">
-											Ваша имя
+											Ваше имя
 										</label>
 										<input
 											type="text"
 											name="name"
 											id="name"
 											className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
-											placeholder="name@email.com"
+											placeholder="Name"
 											ref={nameRef}
 										/>
 									</div>
