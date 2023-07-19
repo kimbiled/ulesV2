67c4cf8cd3ca579dec07f1c9312dc0414c162bf4
Meirambekov Nursultan <meirambekov.n2005@gmail.com>
2023-07-19 20:13:11 +0600

    Fix problems + new design

--- frontend/src/app/customer/page.tsx
+++ frontend/src/app/customer/page.tsx
@@ -1,8 +1,14 @@
+'use client'
+
 import Layout from "@components/Layout/Layout";
 import Image from "next/image";
 import {downarrow, userImg} from '../../../public/assets/index'
+import { useState } from "react";
 
 export default function Customer() {
+	const [isChange, setChange] = useState(false);
+
+
 	const products = [
 		{
 			id:1,
@@ -95,13 +101,16 @@ export default function Customer() {
 	]
 	return (
 		<Layout>
-			<div className="flex flex-row justify-between w-screen mt-16 mb-16 fontRaleway">
+			<div className="fontRaleway flex flex-row justify-between w-screen mt-16 mb-16">
 				<div className="w-[845px] h-auto bg-gradient-linear rounded-3xl flex flex-row justify-between p-8">
 					<div className="h-auto">
 						<div className="bg-volunteerColor w-[465px] h-auto py-4 gap-4 rounded-3xl border-[1px] border-white flex flex-wrap items-center justify-evenly">
+							<div className="w-full text-center">
+								<p className="font-semibold text-xl text-white">Заказ создан 11.07.2023</p>
+							</div>
 							{products.map(item=>(
 								<div key={item.id} className="w-44 h-14 rounded-xl border-[1px] border-white flex flex-col justify-center text-white p-2">
-									<p className="text-[10px]">{item.productType}</p>
+									<p className="text-[10px]">Количество продукта</p>
 									<p className="text-sm">{item.productName}</p>
 								</div>
 							))}
@@ -144,7 +153,7 @@ export default function Customer() {
 				</div>
 
 				<div className="w-[336px] h-auto bg-gradient-linear rounded-3xl text-white p-4">
-					<div className="flex flex-col gap-4 justify-center w-[280px] justify-center m-auto">
+					<div className="flex flex-col gap-5 justify-center w-[280px] justify-center m-auto">
 						<div className="flex flex-row  justify-start gap-4 items-center">
 							<div className="w-[72px] h-[72px] rounded-full bg-gray-400"></div>
 							<div>
@@ -155,10 +164,13 @@ export default function Customer() {
 						<div className="w-auto h-auto rounded-xl border-[1px] border-white p-4">
 							<p className="text-xs">Список желаемых продуктов</p>
 							<div className="">
-								<ul className="flex flex-wrap text-sm font-medium list-disc list-inside">
+								<ul className="flex flex-wrap text-sm font-medium list-disc list-inside items-center gap-1">
 									{wanterProducts.map(item=>(
 										<>
-											<li className="w-20 " key={item.id}>{item.product}</li>
+											<li className="w-20" key={item.id}>{item.product}</li>
+											<div className="w-7 h-2.5 border-white border-[1px] rounded-3xl bg-organisationInput flex justify-center items-center">
+												<p className="font-semibold text-[8px]">0.1 кг</p>
+											</div>
 										</>
 									))}
 								</ul>
@@ -172,14 +184,31 @@ export default function Customer() {
 							<p className="text-[10px]">Возраст</p>
 							<p className="text-sm">56 лет</p>
 						</div>
+						{isChange? 
+						(
 						<div className="w-auto h-11 rounded-xl border-[1px] border-white flex flex-col justify-center text-white p-4">
-							<p className="text-[10px]">Частота доставки</p>
-							<p className="text-sm">1-2 раза в неделю</p>
+							<p className="text-[10px]">Ваш адрес</p>
+							<input type="text" className="bg-transparent outline-none text-sm" placeholder="Кабайбай Батыра 51/1"/>
 						</div>
+						):
+						(
 						<div className="w-auto h-11 rounded-xl border-[1px] border-white flex flex-col justify-center text-white p-4">
 							<p className="text-[10px]">Ваш адрес</p>
 							<p className="text-sm">Кабанбай Батыра 51/1</p>
 						</div>
+						)
+					}
+
+						{isChange? (
+							<div className="flex w-full justify-center ">
+								<button className="fontInter hover:bg-stone-200 w-52 h-8 bg-white text-black rounded-3xl mb-1 mt-1 text-sm" onClick={()=>setChange(false)}>Сохранить</button>
+							</div>
+						):
+						(
+						<div className="flex w-full justify-center ">
+							<button className="fontInter hover:bg-stone-200 w-52 h-8 bg-white text-black rounded-3xl mb-1 mt-1 text-sm" onClick={()=>setChange(true)}>Изменить</button>
+						</div>
+						)}
 					</div>
 				</div>
 			</div>
