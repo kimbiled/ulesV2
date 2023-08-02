2cbd8ef2aeab37996eed5536c0203da97caa6a79
yungkhann <ertaev.19102007@gmail.com>
2023-08-02 18:47:17 +0600

    fail

--- frontend/src/app/volunteer/page.tsx
+++ frontend/src/app/volunteer/page.tsx
@@ -1,14 +1,14 @@
-import Volunteer from "@routes/volunteer/volunteer";
+import Volunteer from '@routes/volunteer/volunteer';
 
-import { VolunteerProvider } from "@context/Volunteer/useVolunteer";
-import ProtectedRoute from "@components/ProtectedRoute/ProtectedRoute";
+import { VolunteerProvider } from '@context/Volunteer/useVolunteer';
+import ProtectedRoute from '@components/ProtectedRoute/ProtectedRoute';
 
 export default function page() {
-	return (
-		<ProtectedRoute>
-			<VolunteerProvider>
-				<Volunteer />
-			</VolunteerProvider>
-		</ProtectedRoute>
-	);
+  return (
+    <ProtectedRoute>
+      <VolunteerProvider>
+        <Volunteer />
+      </VolunteerProvider>
+    </ProtectedRoute>
+  );
 }
