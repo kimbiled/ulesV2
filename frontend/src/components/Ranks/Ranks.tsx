3da3bef0619bf1564cc1ea408dda20f57bde1d8a
yungkhann <ertaev.19102007@gmail.com>
2023-08-05 00:15:32 +0600

    landing adaptive

--- frontend/src/components/Ranks/Ranks.tsx
+++ frontend/src/components/Ranks/Ranks.tsx
@@ -3,42 +3,42 @@ import React from 'react';
 import Image from 'next/image';
 const Ranks = () => {
   return (
-    <div className=" fontRaleway flex justify-center items-center space-x-10 mx-auto max-w-screen-xl px-4 py-12 sm:px-6 md:py-16 lg:px-8 ">
-      <div className="space-y-10 max-w-xl">
-        <h2 className=" text-4xl font-bold  sm:text-5xl text-ulsDark">
+    <div className=" fontRaleway flex flex-col lg:flex-row justify-center items-center lg:space-x-10 mx-auto max-w-screen-xl px-4 py-12 sm:px-6 md:py-16 lg:px-8 ">
+      <div className="space-y-5 md:space-y-10 max-w-xl ">
+        <h2 className=" text-2xl md:text-4xl font-bold text-left  text-ulsDark">
           Лидеры рейтинга социальной помощи
         </h2>
-        <p>
+        <p className="text-xs lg:text-md font-medium ">
           Нет никого, кто любил бы боль саму по себе, кто искал бы её и кто
           хотел бы иметь её просто потому, что это боль..
         </p>
-        <button className="hover:bg-gray-100 hover:text-ulsDark duration-300 ease-in-out  items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center   text-white rounded-lg  bg-ulsDark  ">
+        <button className="hover:bg-gray-100 hover:text-ulsDark duration-300 ease-in-out  items-center justify-center px-3 py-2 md:px-5 md:py-3 mr-3 text-sm md:text-base font-medium text-center   text-white rounded-lg  bg-ulsDark  ">
           Таблица
         </button>
       </div>
 
       <div className="mt-12 flex flex-col space-y-10">
-        <blockquote className="rounded-lg bg-white p-8 w-[550px]">
+        <blockquote className="rounded-lg bg-white p-4 lg:p-8 max-w-xl">
           <div className="flex items-center gap-4">
             <Image
               src="https://images.unsplash.com/photo-1595152772835-219674b2a8a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
               alt="mockup"
-              width={50}
-              height={50}
-              className="rounded-full object-cover overflow-hidden"
+              width={25}
+              height={25}
+              className="rounded-full w-[40px] lg:w-[50px] object-cover overflow-hidden"
             />
 
             <div>
-              <p className="mt-1 text-lg font-medium text-ulsDark">
+              <p className="mt-1  text-base lg:text-lg font-medium text-ulsDark">
                 Санжар Кыдырбеков
               </p>
-              <p className=" text-md font-medium text-gray-500">
+              <p className=" text-xs lg:text-md font-medium text-gray-500">
                 180 Ules поинт
               </p>
             </div>
           </div>
 
-          <p className="line-clamp-2 sm:line-clamp-none mt-4 text-ulsDark">
+          <p className="text-sm md:text-md mt-4 text-ulsDark">
             Санжар является активным участником сообщества Ules и внес
             значительный вклад в поддержку социально-уязвимых категорий
             населения. Его щедрые пожертвования и непрерывное участие в
@@ -47,25 +47,27 @@ const Ranks = () => {
           </p>
         </blockquote>
 
-        <blockquote className="rounded-lg bg-white p-8 w-[550px]">
+        <blockquote className="rounded-lg bg-white p-4 lg:p-8 max-w-xl">
           <div className="flex items-center gap-4">
             <Image
               alt="mockup"
-              width={50}
-              height={50}
               src={magnum}
-              className=" rounded-full object-cover overflow-hidden"
+              width={25}
+              height={25}
+              className=" rounded-full w-[40px] lg:w-[50px] object-cover overflow-hidden"
             />
 
             <div>
-              <p className="mt-1 text-lg font-medium text-ulsDark">Magnum</p>
-              <p className=" text-md font-medium text-gray-500">
+              <p className="mt-1  text-base lg:text-lg font-medium text-ulsDark">
+                Magnum
+              </p>
+              <p className=" text-xs lg:text-md font-medium text-gray-500">
                 180 Ules поинт
               </p>
             </div>
           </div>
 
-          <p className="line-clamp-2 sm:line-clamp-none mt-4 text-ulsDark">
+          <p className="text-sm md:text-md mt-4 text-ulsDark">
             Magnum - еще один важный участник сообщества Ules, который проявляет
             постоянную активность в поддержке нуждающихся. Его щедрость и
             энергия привносят надежду в жизни тех, кто нуждается в помощи.
--- frontend/src/components/Stats/Stats.tsx
+++ frontend/src/components/Stats/Stats.tsx
@@ -4,29 +4,30 @@ import Image from 'next/image';
 const Stats = () => {
   return (
     <section className=" fontRaleway ">
-      <div className="mx-auto max-w-screen-xl px-4 py-12 sm:px-6 md:py-16 lg:px-8">
+      <div className="mx-auto  px-2 py-12 sm:px-6 md:py-16 lg:px-8">
         <div className="mx-auto max-w-3xl text-center">
-          <h2 className="text-3xl font-bold text-ulsDark sm:text-4xl">
+          <h2 className="text-2xl md:text-4xl font-bold text-ulsDark sm:text-4xl">
             Статистика
           </h2>
         </div>
 
         <div className="mt-8 sm:mt-12">
-          <dl className="grid grid-cols-1 gap-8 sm:grid-cols-4">
+          <dl className="px-12 lg:px-5 grid grid-cols-1 gap-8  sm:grid-cols-4">
             {stats.map(item => (
               <div
                 key={item.id}
-                className="shadow-md bg-white transform transition-all hover:scale-105 hover:shadow-xl flex flex-col space-y-4 justify-center items-center rounded-3xl border border-gray-100 px-4 py-8 text-center ">
+                className=" shadow-md bg-white transform transition-all hover:scale-105 hover:shadow-xl flex flex-col space-y-2 md:space-y-4 justify-center items-center rounded-3xl border border-gray-100 px-2 py-4 md:px-4 md:py-8 text-center ">
                 <Image
                   alt="item"
-                  width={100}
-                  height={100}
+                  width={50}
+                  height={50}
+                  className="h-[50px] w-[50px] md:h-[100px] md:w-[100px]"
                   src={item.img}></Image>
-                <dt className="order-last text-sm font-medium text-gray-500">
+                <dt className="order-last text-xs md:text-sm font-medium text-gray-500">
                   {item.value}
                 </dt>
 
-                <dd className=" font-extrabold text-ulsDark text-xl">
+                <dd className=" font-extrabold text-ulsDark text-sm md:text-xl">
                   {item.title}
                 </dd>
               </div>
