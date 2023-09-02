e6aa47758037fc06ecc19670db6891d3c98de18c
yungkhann <ertaev.19102007@gmail.com>
2023-09-02 23:39:53 +0600

    danial noob

--- frontend/src/hooks/user/types/index.ts
+++ frontend/src/hooks/user/types/index.ts
@@ -1,11 +1,11 @@
 export type TUser = {
-	name: string;
-	email: string;
-	phone: string;
-	user_type: number;
-	company: string;
-	address: string;
-	rating: number;
-	norm: number;
-	rank: number;
+  name: string;
+  email: string;
+  phone: string;
+  user_type: number;
+  company: string;
+  address: string;
+  rating: number;
+  norm: string;
+  rank: number;
 };
